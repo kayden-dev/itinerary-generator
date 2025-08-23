cc77b8c5187b4d1496f7d296bef72a664ba630fc
Kayden Nguyen <kngu0087@student.monash.edu>
2025-08-23 19:21:09 +1000

    add files for C1 to implement tests with student information

---
+++ C1/test_can_borrow.py
@@ -0,0 +1,19 @@
+'''
+The method you are testing is:
+    can_borrow(item_type, patron_age, length_of_loan, outstanding_fees, gardening_training, carpentry_training)
+
+The data type of each parameter is:
+- item_type: string
+- patron_age: integer
+- length_of_loan: integer
+- outstanding_fees: float
+- gardening_training: boolean
+- carpentry_training: boolean
+
+You can assume that the can_borrow method is already imported into this python module,
+so you can call "can_borrow" directly.
+
+Author: Kayden Nguyen
+Student ID: 33878935
+'''
+
---
+++ C1/test_discount.py
@@ -0,0 +1,13 @@
+'''
+The method you are testing is:
+    calculate_discount(age)
+
+The data type of each parameter is:
+- age: integer
+
+You can assume that the calculate_discount method is already imported into this python module,
+so you can call "calculate_discount" directly.
+
+Author: Kayden Nguyen
+Student ID: 33878935
+'''
