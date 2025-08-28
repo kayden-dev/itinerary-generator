ce0b93a465fe9babb253c1426eb6e3b240525fcb
Kayden Nguyen <kngu0087@student.monash.edu>
2025-08-28 21:47:17 +1000

    create tests for sum method

--- statistics_tdd_activity/tests/test_statistics.py
+++ statistics_tdd_activity/tests/test_statistics.py
@@ -3,5 +3,17 @@ import unittest
 from src.statistics import *
 
 class TestStatistics(unittest.TestCase):
-    def test_something(self):
-        self.assertTrue(True)
+    def test_sum_positives(self):
+        expected = 21
+        actual = sum([1,2,3,4,5,6])
+        self.assertEqual(expected,actual,"Summing 1-6 should equal 21")
+
+    def test_sum_negatives(self):
+        expected = -21
+        actual = sum([-1,-2,-3,-4,-5,-6])
+        self.assertEqual(expected,actual,"Summing 1-6 (negative) should equal -21")
+
+    def test_sum_mixed(self):
+        expected = 9
+        actual = sum([1,-2,3,-4,5,6])
+        self.assertEqual(expected,actual,"Summing [1,-2,3,-4,5,6] should equal 9")
