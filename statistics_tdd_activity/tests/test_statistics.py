7d48d19186a178cc386a967c98a754c9d70c3dd5
Kayden Nguyen <kngu0087@student.monash.edu>
2025-08-28 22:06:28 +1000

    create tests for mean method

--- statistics_tdd_activity/tests/test_statistics.py
+++ statistics_tdd_activity/tests/test_statistics.py
@@ -17,3 +17,18 @@ class TestStatistics(unittest.TestCase):
         expected = 9
         actual = sum([1,-2,3,-4,5,6])
         self.assertEqual(expected,actual,"Summing [1,-2,3,-4,5,6] should equal 9")
+
+    def test_mean_positives(self):
+        expected = 3.5
+        actual = mean([1,2,3,4,5,6])
+        self.assertEqual(expected, actual, "Mean of 1-6 should equal 3.5")
+
+    def test_mean_negatives(self):
+        expected = -3.5
+        actual = mean([-1,-2,-3,-4,-5,-6])
+        self.assertEqual(expected, actual, "Mean of -1 to -6 should equal -3.5")
+
+    def test_mean_mixed(self):
+        expected = 1.5
+        actual = mean([1,-2,3,-4,5,6])
+        self.assertEqual(expected, actual, "Mean of [1,-2,3,-4,5,6] should equal 1.5")
