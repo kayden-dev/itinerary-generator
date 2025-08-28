24a9d4c6d5728290de09fa72e83763b3b94f5947
Kayden Nguyen <kngu0087@student.monash.edu>
2025-08-28 22:59:14 +1000

    create add method test

--- test_your_knowledge_topic_4/tests/test_calculator.py
+++ test_your_knowledge_topic_4/tests/test_calculator.py
@@ -11,4 +11,11 @@ class TestCalculator(unittest.TestCase):
 
     def test_initial_answer(self):
         calc = Calculator()
-        self.assertEqual(0, calc.get_answer(),"Initial answer should be 0")
+        self.assertEqual(0, calc.get_answer())
+
+    def test_add(self):
+        expected = 5
+        c = Calculator()
+        c.add(5)
+        actual = c.get_answer()
+        self.assertEqual(expected, actual, "Adding 5 to 0 should equal 5")
