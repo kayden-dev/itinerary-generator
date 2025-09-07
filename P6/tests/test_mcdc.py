99b0a5556b2937851b3545e8a1a95ab34c2bb777
Kayden Nguyen <kngu0087@student.monash.edu>
2025-09-07 16:33:32 +1000

    implement p6 mcdc tests and code

--- P6/tests/test_mcdc.py
+++ P6/tests/test_mcdc.py
@@ -1,7 +1,33 @@
 # Student name: Kayden Nguyen
 # Student ID: 33878935
 
-def mcdc(a,b,c,d):
-  if (a and (b or c) and d):
-    return True;
-  return False;
+import unittest
+
+from src.mcdc import mcdc
+
+class TestMcdc(unittest.TestCase):
+  """
+  6: A=F, B=T, C=F, D=T, Outcome=F
+  10: A=T, B=F, C=F, D=T, Outcome=F
+  12: A=T, B=F, C=T, D=T, Outcome=T
+  13: A=T, B=T, C=F, D=F, Outcome=F
+  14: A=T, B=T, C=F, D=T, Outcome=T
+  """
+
+  def setUp(self):
+    pass
+
+  def test_6(self):
+    self.assertFalse(mcdc(False,True,False,True))
+  
+  def test_10(self):
+    self.assertFalse(mcdc(True,False,False,True))
+
+  def test_12(self):
+    self.assertTrue(mcdc(True,False,True,True))
+
+  def test_13(self):
+    self.assertFalse(mcdc(True,True,False,False))
+
+  def test_14(self):
+    self.assertTrue(mcdc(True,True,False,True))
