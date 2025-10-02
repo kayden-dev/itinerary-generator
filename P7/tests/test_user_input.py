a87495e83e98eecd0cc7cd4b3e6eb4719ecbdacb
Kayden Nguyen <kngu0087@student.monash.edu>
2025-10-02 16:24:04 +1000

    write tests to get 100% branch and statement coverage for user input

---
+++ P7/tests/test_user_input.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest import mock
+from src.user_input import *
+
+class TestUserInput(unittest.TestCase):
+
+  @mock.patch('builtins.input')
+  def test_read_string(self,inp):
+    input.return_value = 'hello world!'
+    self.assertEqual('hello world!', read_string('Enter a value: '))
+
+  @mock.patch('src.user_input.read_string')
+  def test_read_integer(self,readstr):
+    readstr.side_effect = ['hello world!', '1']
+    self.assertEqual(1, read_integer('Enter an integer: '))
+
+  @mock.patch('src.user_input.read_string')
+  def test_read_float(self,readstr):
+    readstr.side_effect = ['hello world!', '1.5']
+    self.assertEqual(1.5, read_float('Enter an float: '))
+
+  @mock.patch('src.user_input.read_string')
+  def test_read_integer_range(self,readstr):
+    readstr.side_effect = ['10','3']
+    self.assertEqual(3, read_integer_range('Enter an integer: ',1,5))
+
+  @mock.patch('src.user_input.read_string')
+  def test_read_float_range(self,readstr):
+    readstr.side_effect = ['10.5','3.3']
+    self.assertEqual(3.3, read_float_range('Enter an float ',1.5,5.6))
