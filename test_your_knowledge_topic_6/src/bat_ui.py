53f416a4fee9fcd5238fc055a739b5a3fad67583
Kayden Nguyen <kngu0087@student.monash.edu>
2025-10-03 16:14:15 +1000

    refactor bat ui to lie within 79 column limits

--- test_your_knowledge_topic_6/src/bat_ui.py
+++ test_your_knowledge_topic_6/src/bat_ui.py
@@ -33,8 +33,10 @@ class BatUI():
         Retrieve the current menu screen.
 
             Returns:
-                A string representation of the current menu screen. Possible values are
-                "MAIN MENU", "LOAN ITEM", "RETURN ITEM", "SEARCH FOR PATRON", "REGISTER PATRON",
+                A string representation of the current menu screen. 
+                Possible values are
+                "MAIN MENU", "LOAN ITEM", "RETURN ITEM", 
+                "SEARCH FOR PATRON", "REGISTER PATRON",
                 "ACCESS MAKERSPACE", and "QUIT".
         '''
         match self._current_screen:
@@ -55,14 +57,16 @@ class BatUI():
 
     def run_current_screen(self):
         '''
-        Run the current menu screen. If necessary, transition to a new menu screen.
+        Run the current menu screen.
+          If necessary, transition to a new menu screen.
         '''
         self._current_screen = self._current_screen()
 
     def _main_menu(self):
         '''
-        The main menu screen of BAT. Presents the user with 6 options (loan item, return
-        item, search for patron, register patron, validate makerspace access, and quit),
+        The main menu screen of BAT. Presents the user with 6 options 
+        (loan item, returnitem, search for patron, register patron, 
+        validate makerspace access, and quit),
         and transitions to the menu screen the user selects.
 
         Keeps asking the user for a selection until the enter a valid choice.
@@ -104,15 +108,18 @@ class BatUI():
         - ask the user for the ID of the item to be loaned
         - ask the user to confirm if that is the correct item
         - ask the user for the name and age of the patron loaning the item
-        - ask the user for the length of the loan in days (from 1 - 365 inclusive)
-        - loan the item if the specified user is allowed to loan the specified item
+        - ask the user for the length of the loan in days 
+        (from 1 - 365 inclusive)
+        - loan the item if the specified user is 
+        allowed to loan the specified item
 
-        If the process fails at any point (e.g., an item with the given ID can't
-        be found in the catalogue, a patron with the given name and age can't be
-        found in the patron database), return to the main menu screen.
-
-        If the loan is successfully processed, print a message then return to the
+        If the process fails at any point (e.g., an item with the given 
+        ID can'tbe found in the catalogue, a patron with the given name 
+        and age can't befound in the patron database), return to the
         main menu screen.
+
+        If the loan is successfully processed, print a message then return 
+        to the main menu screen.
         '''
         print("""
             ------------------
@@ -121,7 +128,8 @@ class BatUI():
             """)
 
         item_id = user_input.read_integer('Enter id of item to loan: ')
-        item = search.find_item_by_id(item_id, self._data_manager._catalogue_data)
+        item = search.find_item_by_id(item_id,
+                                       self._data_manager._catalogue_data)
 
         if item == None:
             print("!!! No such item. CANCELLING LOAN.")
@@ -129,22 +137,28 @@ class BatUI():
             print(f"Found {item._type}: {item._name} ({item._year})")
             choice = user_input.read_bool("Is this the item (y/n)? ")
             if choice == 'y':
-                print("Enter details of patron wanting to loan this item...")
+                print(
+                    "Enter details of patron wanting to loan this item...")
                 name = user_input.read_string("Patron's name: ")
                 age = user_input.read_integer("Patron's age: ")
 
-                patron = search.find_patron_by_name_and_age(name, age, self._data_manager._patron_data)
+                patron = search.find_patron_by_name_and_age(
+                    name, age, self._data_manager._patron_data)
 
                 if patron == None:
                     print("!!! NO SUCH PATRON. CANCELLING LOAN.")
                 else:
-                    length_of_loan = user_input.read_integer_range("How many days is the loan for (1 - 365)? ", 1, 365)
-                    loan_success = logic.process_loan(patron, item, length_of_loan)
+                    length_of_loan = user_input.read_integer_range(
+                        "How many days is the loan for (1 - 365)? ", 1, 365)
+                    loan_success = logic.process_loan(
+                        patron, item, length_of_loan)
 
                     if loan_success:
-                        print(f"Loan of {item._name} to {patron._name} successfully recorded")
+                        print(
+                            f"Loan of {item._name} to {patron._name} successfully recorded")
                     else:
-                        print(f"Sorry, {patron._name} is not able to borrow {item._name}")
+                        print(
+                            f"Sorry, {patron._name} is not able to borrow {item._name}")
             else:
                 print("CANCELLING LOAN")
 
@@ -156,15 +170,16 @@ class BatUI():
         The return item menu screen of BAT. Follows the process:
         - ask the user for the name and age of the patron returning the item
         - print a list of loans that user currently has
-        - ask for the item ID of the item being returned, and continue asking until
-        a valid item ID is entered
+        - ask for the item ID of the item being returned, and continue 
+        asking until a valid item ID is entered
         - process the return of the item
 
-        If the process fails at any point (e.g., a patron with the given name and age
-        can't be found in the patron database), return to the main menu screen.
-
-        If the return is successfully processed, print a message then return to the
+        If the process fails at any point (e.g., a patron with the given 
+        name and agecan't be found in the patron database), return to the 
         main menu screen.
+
+        If the return is successfully processed, print a message then return
+        to the main menu screen.
         '''
         print("""
             --------------------
@@ -176,7 +191,8 @@ class BatUI():
         name = user_input.read_string("Patron's name: ")
         age = user_input.read_integer("Patron's age: ")
 
-        patron = search.find_patron_by_name_and_age(name, age, self._data_manager._patron_data)
+        patron = search.find_patron_by_name_and_age(
+            name, age, self._data_manager._patron_data)
 
         if patron == None:
             print("!!! NO SUCH PATRON. CANCELLING RETURN.")
@@ -187,10 +203,14 @@ class BatUI():
                 print(l)
                 loaned_ids.append(l._item._id)
 
-            choice = user_input.read_integer("Enter the ID of the item to return: ")
+            choice = user_input.read_integer(
+                "Enter the ID of the item to return: ")
             while choice not in loaned_ids:
-                print(f"That is not an ID of an item currently loaned by {patron._name}")
-                choice = user_input.read_integer("Enter the ID of the item to return: ")
+                print(
+                    f"That is not an ID of an item currently loaned by {
+                        patron._name}")
+                choice = user_input.read_integer(
+                    "Enter the ID of the item to return: ")
 
             logic.process_return(patron, choice)
             print(f"Return of item from {patron._name} successfully recorded")
@@ -199,9 +219,9 @@ class BatUI():
     
     def _search_for_patron(self):
         '''
-        The patron search menu screen of BAT. Allows users to enter a name or age (one 
-        or the other, not both), and have the details of any patrons with that name or
-        age printed out.
+        The patron search menu screen of BAT. Allows users to enter a 
+        name or age (one or the other, not both), and have the details of any
+        patrons with that name or age printed out.
         '''
         print("""
             --------------------
@@ -220,10 +240,12 @@ class BatUI():
         match choice:
             case 1:
                 name = user_input.read_string("Enter name: ")
-                patrons_found = search.find_patron_by_name(name, self._data_manager._patron_data)
+                patrons_found = search.find_patron_by_name(
+                    name, self._data_manager._patron_data)
             case 2:
                 age = user_input.read_integer("Enter age: ")
-                patrons_found = search.find_patron_by_age(age, self._data_manager._patron_data)
+                patrons_found = search.find_patron_by_age(
+                    age, self._data_manager._patron_data)
             case 3:
                 return self._main_menu
             case _:
@@ -240,9 +262,10 @@ class BatUI():
     
     def _register_patron(self):
         '''
-        The register patron menu screen of BAT. Allows the user to create a new
-        patron for the database. The user is asked for a name, and an age (from 0 to
-        100 inclusive). The new patron will start with no outstanding fees, no loans,
+        The register patron menu screen of BAT. Allows the user to 
+        create a new patron for the database. The user is asked for 
+        a name, and an age (from 0 to 100 inclusive). The new patron
+        will start with no outstanding fees, no loans,
         and no completed training.
         '''
         print("""
@@ -277,12 +300,16 @@ class BatUI():
         name = user_input.read_string("Enter name: ")
         age = user_input.read_integer("Enter age: ")
 
-        patron = search.find_patron_by_name_and_age(name, age, self._data_manager._patron_data)
+        patron = search.find_patron_by_name_and_age(
+            name, age, self._data_manager._patron_data)
 
         if (patron == None):
             print("!!! NO SUCH PATRON")
         else:
-            allowed = logic.can_use_makerspace(patron._age, patron._outstanding_fees, patron._makerspace_training)
+            allowed = logic.can_use_makerspace(
+                patron._age, 
+                patron._outstanding_fees, 
+                patron._makerspace_training)
             if allowed:
                 print(f"{patron._name} is allowed to use the makerspace")
             else:
